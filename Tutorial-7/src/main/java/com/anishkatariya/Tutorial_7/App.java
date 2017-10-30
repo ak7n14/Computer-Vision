65439f20394f2dfa8f1024a58c92e5f154db8c1a
Anish Katariya <anishkatariya@Anishs-MacBook-Pro.local>
2017-10-30 13:34:58 +0000

    Finished Tutorial 7

---
+++ Tutorial-7/src/main/java/com/anishkatariya/Tutorial_7/App.java
@@ -0,0 +1,51 @@
+package com.anishkatariya.Tutorial_7;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openimaj.image.DisplayUtilities;
+import org.openimaj.image.MBFImage;
+import org.openimaj.image.colour.ColourSpace;
+import org.openimaj.image.colour.RGBColour;
+import org.openimaj.image.processing.convolution.FGaussianConvolve;
+import org.openimaj.image.processing.edges.CannyEdgeDetector;
+import org.openimaj.image.processing.edges.*;
+import org.openimaj.image.processing.edges.*;
+import org.openimaj.image.typography.hershey.HersheyFont;
+import org.openimaj.video.*;
+import org.openimaj.video.capture.VideoCapture;
+import org.openimaj.video.xuggle.XuggleVideo;
+
+
+/**
+ * OpenIMAJ Hello world!
+ *
+ */
+public class App {
+    public static void main( String[] args ) throws IOException {
+    	Video<MBFImage> video;
+    	video = new XuggleVideo(new File("Data/keyboardcat.flv"));
+   	//video = new VideoCapture(320, 240);
+//    	VideoDisplay<MBFImage> display = VideoDisplay.createVideoDisplay(video);
+  	
+//    	for (MBFImage mbfImage : video) {
+//    	    DisplayUtilities.displayName(mbfImage.process(new CannyEdgeDetector()), "videoFrames");
+//    	}
+    	   
+    
+
+    	VideoDisplay<MBFImage> display1 = VideoDisplay.createVideoDisplay(video);
+    	display1.addVideoListener(
+    	  new VideoDisplayListener<MBFImage>() {
+    	    @SuppressWarnings("deprecation")
+			public void beforeUpdate(MBFImage frame) {
+    	        frame.processInplace(new CannyEdgeDetector2());
+    	    }
+
+    	    public void afterUpdate(VideoDisplay<MBFImage> display1) {
+    	    }
+    	  });
+    }
+}
